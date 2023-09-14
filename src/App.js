e320b07e9a1d70b0ab611023965ce8a4eb8f2420
LeeDahee <dduosy9@gmail.com>
2023-09-14 11:13:37 +0900

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import Lists from './components/Lists';
 import Form from './components/Form';
@@ -21,6 +21,15 @@ export default function App() {
     setValue('');
   };
 
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      console.log('newTodoData', newTodoData);
+      setTodoData(newTodoData);
+    },
+    [todoData]
+  );
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
@@ -28,7 +37,11 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
 
-        <Lists todoData={todoData} setTodoData={setTodoData} />
+        <Lists
+          handleClick={handleClick}
+          todoData={todoData}
+          setTodoData={setTodoData}
+        />
 
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
